504b416c29be0b63f9bd8f3cfe5ce59d9b6a0a8a
cybertino <istodim@gmail.com>
2024-05-30 00:35:04 +0930

    Created basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
 import Image from "next/image";
 
+import "./page.scss";
+
 export default function Home() {
-  return <main className=''>Hello World!</main>;
+  return (
+    <>
+      <header className='topbar'>Art Gallery</header>
+
+      <main>
+        <aside className='sidebar'></aside>
+
+        <div className='content-container'>
+          <div className='content'>
+            {Array(100)
+              .fill(0)
+              .map((_, index) => (
+                <Tile key={index} />
+              ))}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
+
+const Tile = () => (
+  <div className='tile'>
+    <div className='tile-body'></div>
+  </div>
+);
